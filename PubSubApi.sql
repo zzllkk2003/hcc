c3b93de0025c14ba19205b3cff472a01e91036c9
Jeff Liu <jasonliuspark@outlook.com>
2021-08-03 01:49:38 +0800

    增加 new subscriber接口

---
+++ PubSubApi.sql
@@ -0,0 +1,14 @@
+SELECT * from Ens_Config.Production 
+
+select * FROM Ens_Config.Item 
+
+
+SELECT ID,Name,Description
+FROM EnsLib_PubSub.DomainName
+ORDER BY Name
+
+SELECT * FROM  EnsLib_PubSub.Subscriber 
+
+SELECT * FROM  EnsLib_PubSub.Subscription  
+
+select * from  
