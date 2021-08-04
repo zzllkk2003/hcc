6c3517a89a4201a8f87fce99432bd928ea161388
Jeff Liu <jasonliuspark@outlook.com>
2021-08-04 23:55:26 +0800

    增加all subscriber 接口

--- PubSubApi.sql
+++ PubSubApi.sql
@@ -10,3 +10,5 @@ ORDER BY Name
 SELECT * FROM  EnsLib_PubSub.Subscriber 
 
 SELECT * FROM  EnsLib_PubSub.Subscription  
+
+select a.DomainName ,a.Name ,a.Target, b.Topic,c.ClassName FROM  EnsLib_PubSub.Subscriber as a Left join EnsLib_PubSub.Subscription as b on a.ID = b.Subscriber inner join Ens_Config.Item as c on a.Target = c.Name where c.ClassName ='HCC.SVR.Prod.BO.StandardHCCOutBound'
